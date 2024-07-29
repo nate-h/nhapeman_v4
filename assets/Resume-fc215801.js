503083e5c67a4497a9798e53dac10c045d555967
nates linux desktop <nhapeman@gmail.com>
2024-07-28 18:33:58 -0700

    Updates

---
+++ assets/Resume-fc215801.js
@@ -0,0 +1,1 @@
+import{_ as e,o as s,c as o,a as c}from"./index-6d2acf51.js";const t={},_={class:"resume"},r=c("iframe",{src:"https://drive.google.com/file/d/1AFZIw1svApTUGUfAyW4PzXYiI-0ESZQc/preview",class:"pdf",frameBorder:"0",scrolling:"auto"},null,-1),a=[r];function n(i,l){return s(),o("div",_,a)}const f=e(t,[["render",n]]);export{f as default};
